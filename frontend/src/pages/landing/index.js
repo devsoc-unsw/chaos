78b5f7232d4b3102cf9b3b0cc104cf70800b7fbc
SpanishPear <shrey.somaiya@gmail.com>
2021-11-29 00:40:51 +1100

    test hook

--- frontend/src/pages/landing/index.js
+++ frontend/src/pages/landing/index.js
@@ -4,8 +4,7 @@ const Landing = () => {
   // eslint-disable-next-line no-unused-vars
   const [loggedIn, setLoggedIn] = React.useState(false);
 
-  return (
-    <div>
+  return ( <div>
       <h1>Landing Page</h1>
     </div>
   );
