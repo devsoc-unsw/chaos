5f12f63cded459902b5c0c205ccea1b0e54a6669
Shrey Somaiya <shrey.somaiya@gmail.com>
2021-12-06 01:13:20 +1100

    initial backend skeleton + some google SSO

---
+++ backend/src/auth/passportGoogleSSO.js
@@ -0,0 +1,62 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+const User = require("../models/user");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      passReqToCallback: true,
+    },
+    async (req, accessToken, refreshToken, profile, doneCallback) => {
+      console.log("GoogleStrategy Executed");
+
+      const defaultUser = {
+        fullName: `${profilegiven_name} ${profile.family_name}`,
+        displayName: profile.displayName,
+        email: profile.emails[0].value,
+        googleId: profile.id,
+        avatar: profile.photos[0].value,
+      };
+
+  await User.findOrCreate({
+          where: { googleId: profile.id }, 
+          defaults: defaultUser
+      })
+        .catch(err => {
+          console.error("Error Serializing", err);
+          return doneCallback(err);
+        })
+        .then(([user, created]) => {
+          console.group("NEW USER")
+          console.log(user);
+          console.log(created);
+          console.groupEnd("NEW USER")
+          return doneCallback(null, user);
+        });
+
+
+    }
+  )
+);
+
+passport.serializeUser((user, doneCallback) => {
+  console.log('Serializing user: ', user);
+  doneCallback(null, user.id);
+});
+
+passport.deserializeUser((id, doneCallback)=> {
+  
+  await findOne({
+    where: { id }
+  }).catch (err => {
+    console.error("Error Deserializing", err);
+    return doneCallback(err, null);
+  }).then(user => {
+    console.log('Deserialized user: ', user);
+    return doneCallback(null, user);
+  });
+})
---
+++ backend/src/models/user.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../database");
+
+const User = sequelize.define("User", {
+  fullName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  displayName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  googleId: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  avatar: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+});
+
+module.exports = User;
