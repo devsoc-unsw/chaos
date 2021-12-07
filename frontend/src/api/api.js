bae4f1f3140cbaaf8d7585e4cb6af8b93ff22f21
SpanishPear <shrey.somaiya@gmail.com>
2021-12-08 01:35:30 +1100

    CHAOS-7 init auth stuff -- needs lots of fixing

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,22 +2,22 @@ const API_URL = process.env.REACT_APP_API_BASE_URL;
 
 // takes in an object
 const API = {
-  request: async ({ path, body, header, queries = {}, method = 'GET' }) => {
+  request: async ({ path, body, header, queries = {}, method = "GET" }) => {
+    console.log("inside req");
     const endpoint = new URL(`${API_URL}${path}`);
     endpoint.search = new URLSearchParams(queries);
 
     const payload = {
       method,
       headers: {
-        'Content-Type': 'applieation/json',
+        "Content-Type": "application/json",
         ...header,
       },
     };
-    if (method !== 'GET') payload.body = JSON.stringify(body);
+    if (method !== "GET") payload.body = JSON.stringify(body);
 
     return fetch(endpoint, payload);
   },
 };
 
 export default API;
-
--- frontend/src/pages/auth_success/index.js
+++ frontend/src/pages/auth_success/index.js
@@ -1,11 +1,54 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { authenticate } from "../../api";
+import useQuery from "../../hooks/useQuery";
 
 const AuthSuccess = () => {
-  console.log("AuthSuccess");
-  return (
-    <div>
-      <h1>Auth Success</h1>
-    </div>
+  const query = useQuery();
+  const [state, setState] = React.useState({
+    isAuthenticated: false,
+    isAuthenticating: true,
+    error: null,
+  });
+
+  useEffect(() => {
+    async function attemptAuth() {
+      const code = query.get("code");
+      console.log(code);
+      if (code) {
+        await authenticate(code)
+          .then((res) => res.json())
+          .then(({ token }) => {
+            localStorage.setItem("AUTH_TOKEN", token);
+            setState({
+              isAuthenticated: true,
+              isAuthenticating: false,
+              error: null,
+            });
+          })
+          .catch((error) => {
+            setState({
+              isAuthenticated: false,
+              isAuthenticating: false,
+              error,
+            });
+          });
+      }
+    }
+
+    attemptAuth();
+  }, []);
+
+  return state.isAuthenticating ? (
+    <div>Authenticating...</div>
+  ) : state.isAuthenticated ? (
+    <div> success</div>
+  ) : (
+    <>
+      <div>
+        <h1>Authentication failed</h1>
+        <p>{state.error.message}</p>
+      </div>
+    </>
   );
 };
 
