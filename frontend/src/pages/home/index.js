9dee9294b809a7362004ee72ce47091ec92fb63a
Hayes Choy <hayeselnut2127@gmail.com>
2021-12-07 23:10:52 +1100

    home page

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 
+const dummyData = {
+  title: "CSESoc Subcommittee Recruitment Drive"
+}
+
 const Home = () => {
   console.log("Home");
   return (
     <div>
-      <h1>Home</h1>
+      <h2>Your Campaigns</h2>
+      <h2>Available Campaigns</h2>
+      <h2>Past Campaigns</h2>
     </div>
   );
 };
