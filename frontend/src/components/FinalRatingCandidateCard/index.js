e23517f2313b4e023606e677f7d1faa277c26cb9
Hayes Choy <hayeselnut2127@gmail.com>
2021-12-22 16:29:39 +1100

    added colours to avg chip

--- frontend/src/components/FinalRatingCandidateCard/index.js
+++ frontend/src/components/FinalRatingCandidateCard/index.js
@@ -16,13 +16,25 @@ import {
 } from "./finalRatingCandidateCard.styled";
 
 const calculateAvg = (ratings) => {
+  if (ratings.length === 0) return 0;
+
   const sum = ratings.map((a) => a.rating).reduce((a, b) => a + b, 0);
-  const avg = sum / ratings.length || 0;
-  return Math.round(avg * 10) / 10;
+  const avg = sum / ratings.length;
+  return (Math.round(avg * 10) / 10).toFixed(1);
+};
+
+const avgColor = (avg) => {
+  if (avg < 1) return "error";
+  if (avg < 2) return "warning";
+  if (avg < 3) return "secondary";
+  if (avg < 4) return "info";
+  return "success";
 };
 
 const FinalRatingCandidateCard = (props) => {
   const { name, position, ratings } = props;
+
+  const avg = calculateAvg(ratings);
 
   return (
     <CandidateCard variant="outlined">
@@ -30,7 +42,7 @@ const FinalRatingCandidateCard = (props) => {
         <CardContent>
           <Grid container alignItems="center">
             <Grid item>
-              <AvgChip label={calculateAvg(ratings)} />
+              <AvgChip label={avg} color={avgColor(avg)} />
             </Grid>
             <Grid item style={{ flexGrow: 1 }}>
               <Typography variant="h6">{name}</Typography>
--- frontend/src/pages/final_rating/index.js
+++ frontend/src/pages/final_rating/index.js
@@ -2,12 +2,6 @@ import { Container } from "@mui/material";
 import React from "react";
 import FinalRatingCandidateCard from "../../components/FinalRatingCandidateCard";
 
-const dummyRatings = [
-  { rater: "Shrey Somaiya", rating: 4 },
-  { rater: "Michael Gribben", rating: 5 },
-  { rater: "Haley Gu", rating: 5 },
-];
-
 const FinalRating = () => (
   <Container>
     <h1>Choose candidates to progress to the next stage</h1>
@@ -15,17 +9,47 @@ const FinalRating = () => (
     <FinalRatingCandidateCard
       name="Hayes Choy"
       position="Student Experience Director"
-      ratings={dummyRatings}
+      ratings={[
+        { rater: "Shrey Somaiya", rating: 5 },
+        { rater: "Michael Gribben", rating: 5 },
+        { rater: "Haley Gu", rating: 5 },
+      ]}
     />
     <FinalRatingCandidateCard
       name="Giuliana De Bellis"
       position="Student Experience Director"
-      ratings={dummyRatings}
+      ratings={[
+        { rater: "Shrey Somaiya", rating: 4 },
+        { rater: "Michael Gribben", rating: 4 },
+        { rater: "Haley Gu", rating: 3 },
+      ]}
     />
     <FinalRatingCandidateCard
       name="Colin Hon"
       position="Student Experience Director"
-      ratings={dummyRatings}
+      ratings={[
+        { rater: "Shrey Somaiya", rating: 2 },
+        { rater: "Michael Gribben", rating: 2 },
+        { rater: "Haley Gu", rating: 2 },
+      ]}
+    />
+    <FinalRatingCandidateCard
+      name="Lachlan Ting"
+      position="Student Experience Director"
+      ratings={[
+        { rater: "Shrey Somaiya", rating: 1 },
+        { rater: "Michael Gribben", rating: 2 },
+        { rater: "Haley Gu", rating: 1 },
+      ]}
+    />
+    <FinalRatingCandidateCard
+      name="Evan Lee"
+      position="Student Experience Director"
+      ratings={[
+        { rater: "Shrey Somaiya", rating: 0 },
+        { rater: "Michael Gribben", rating: 0 },
+        { rater: "Haley Gu", rating: 1 },
+      ]}
     />
   </Container>
 );
