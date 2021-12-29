c19cadce67f518363b34fff51d68610f2b1544b2
Hayes Choy <hayeselnut2127@gmail.com>
2021-12-29 20:34:05 +1100

    expand functionality + dummy data

--- frontend/src/components/CampaignCard/index.js
+++ frontend/src/components/CampaignCard/index.js
@@ -1,38 +1,92 @@
 import React, { useState } from "react";
-import PropTypes from 'prop-types';
-import { Button, Card, CardContent, CardActions, CardMedia, Typography } from "@mui/material";
+import PropTypes from "prop-types";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  CardActionArea,
+  Collapse,
+  CardActions,
+  Button,
+  IconButton,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { ExpandIconButton } from "./campaignCard.styled";
+
+// const ExpandMore = styled((props) => {
+//   const { expand, ...other } = props;
+//   return <IconButton {...other} />;
+// })(({ theme, expand }) => ({
+//   transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+//   marginLeft: "auto",
+//   transition: theme.transitions.create("transform", {
+//     duration: theme.transitions.duration.shortest,
+//   }),
+// }));
 
 const CampaignCard = (props) => {
-  const { title, description, startDate, endDate, img } = props;
+  const [expanded, setExpanded] = useState(false);
+  const { title, appliedFor, positions, startDate, endDate, img } = props;
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
   return (
-    <Card sx={{ maxWidth: 275 }}>
-      <CardMedia
-        component="img"
-        height="140"
-        image={img}
-        alt={`campaign cover for ${title}`}
-      />
-      <CardContent>
-        <Typography variant="h5" component="div">
-          {title}
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          {`${startDate} - ${endDate}`}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Apply</Button>
+    <Card>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={img}
+          alt={`campaign cover for ${title}`}
+        />
+        <CardContent>
+          <Typography variant="overline" display="block">
+            {`${startDate} - ${endDate}`}
+          </Typography>
+          <Typography variant="h6">{title}</Typography>
+        </CardContent>
+      </CardActionArea>
 
+      <CardActions>
+        <Button size="small" variant="outlined">
+          Apply
+        </Button>
+        <ExpandIconButton
+          expanded={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandIconButton>
       </CardActions>
+
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          {positions.map((position) => (
+            <Typography>{`${position.number}x ${position.name}`}</Typography>
+          ))}
+        </CardContent>
+      </Collapse>
     </Card>
   );
 };
 
 CampaignCard.propTypes = {
-  title: PropTypes.string,
-  description: PropTypes.string,
-  img: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  appliedFor: PropTypes.arrayOf(PropTypes.string).isRequired,
+  positions: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      number: PropTypes.number,
+    })
+  ).isRequired,
+  startDate: PropTypes.string.isRequired,
+  endDate: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
 };
 
 export default CampaignCard;
