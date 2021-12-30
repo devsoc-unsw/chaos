922f0728dd7146fd1de5dd81a025caca4b066738
SpanishPear <shrey.somaiya@gmail.com>
2021-12-30 14:26:03 +1100

    remove js backend + test webhook

--- backend_js/src/index.js
+++
@@ -1,6 +0,0 @@
-const app = require("./app");
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Listening: http://localhost:${port}`);
-});
--- backend_js/src/middleware/auth.js
+++
@@ -1,9 +0,0 @@
-module.exports.isUserAuthenticated = (req, res, next) => {
-  if (req.user) {
-    next();
-  } else {
-    res.status(401).send({
-      message: 'You are not authorized to access this resource',
-    });
-  }
-};
