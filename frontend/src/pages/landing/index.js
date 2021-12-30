88bf0e1a12e3eee46369e286af1e930af27c73de
Shrey Sudhir <shrey.somaiya@gmail.com>
2021-12-31 01:07:12 +1100

    CHAOS-6 Login Flow (#9)
    * CHAOS-6 Fix login, with right commits
    
    * CHAOS-6 fix login backend
    
    * update state per: review

--- frontend/src/pages/landing/index.js
+++ frontend/src/pages/landing/index.js
@@ -4,12 +4,17 @@ import { Box } from "@mui/system";
 import { BackgroundWrapper, ParticleWallpaper } from "../../components";
 import { BoldTitle, Subtitle } from "./landing.styled";
 import { SetNavBarTitleContext } from "../../App";
+import { getStore } from "../../utils";
+import { useNavigate } from "react-router-dom";
 
-const OAUTH_CALLBACK_URL = process.env.REACT_APP_OAUTH_CALLBACK_URL;
+const OAUTH_CALLBACK_URL =
+  getStore("AUTH_TOKEN") || process.env.REACT_APP_OAUTH_CALLBACK_URL;
 
 const Landing = () => {
   // eslint-disable-next-line no-unused-vars
   const [loggedIn, setLoggedIn] = React.useState(false);
+  const navigate = useNavigate();
+
   const setNavBarTitle = useContext(SetNavBarTitleContext);
   useEffect(() => {
     setNavBarTitle("");
@@ -23,7 +28,11 @@ const Landing = () => {
         Recruitment Drives, without the fuss.
       </Subtitle>
       <Box>
-        <Button href={OAUTH_CALLBACK_URL}>Get Started</Button>
+        {getStore("AUTH_TOKEN") ? (
+          <Button onClick={() => navigate("/dashboard")}>Your Dashboard</Button>
+        ) : (
+          <Button href={OAUTH_CALLBACK_URL}> Get Started </Button>
+        )}
       </Box>
     </BackgroundWrapper>
   );
