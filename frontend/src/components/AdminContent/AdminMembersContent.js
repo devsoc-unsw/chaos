44e2c769fc79932022854663d0d4903a9394a415
stapletm <89827883+stapletm@users.noreply.github.com>
2022-01-26 15:01:42 +1100

    Added campaigns + members content to admin page (#35)
    * added content to admin page + links to create/delete campaigns
    
    * fixed style issues
    
    * further style fixes (BACKTICKS)
    
    Co-authored-by: Lewis Cullen <Lewis@Lewies-MBP.hub>

---
+++ frontend/src/components/AdminContent/AdminMembersContent.js
@@ -0,0 +1,64 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+import { ListItemText, Divider, ListItemIcon, IconButton } from "@mui/material";
+import ClearIcon from "@mui/icons-material/Clear";
+import EditIcon from "@mui/icons-material/Edit";
+import AddIcon from "@mui/icons-material/Add";
+import {
+  AdminContentList,
+  DummyIconForAlignment,
+  ContentListHeader,
+  AdminDivider,
+  MemberListItem,
+  AdminListItemButton,
+} from "./adminContent.styled";
+
+const AdminMembersContent = ({ members, setMembers }) => {
+  const onDelete = (memberId) => {
+    // FIXME: CHAOS-55, integrate with backend to actually delete
+    setMembers(members.filter((m) => m.id !== parseInt(memberId, 10)));
+  };
+  return (
+    <AdminContentList>
+      <ContentListHeader>
+        <ListItemText align="center">Name</ListItemText>
+        <ListItemText align="center">Role</ListItemText>
+        <ListItemIcon>
+          <DummyIconForAlignment />
+        </ListItemIcon>
+        <ListItemIcon>
+          <IconButton>
+            <AddIcon />
+          </IconButton>
+        </ListItemIcon>
+      </ContentListHeader>
+      <AdminDivider />
+      {members.map((m) => (
+        <div>
+          <MemberListItem>
+            <AdminListItemButton>
+              <ListItemText align="center">{m.name}</ListItemText>
+              <ListItemText align="center">{m.role}</ListItemText>
+              <ListItemIcon>
+                <IconButton>
+                  <EditIcon />
+                </IconButton>
+              </ListItemIcon>
+              <ListItemIcon>
+                <IconButton
+                  value={m.id}
+                  onClick={(e) => onDelete(e.currentTarget.value)}
+                >
+                  <ClearIcon />
+                </IconButton>
+              </ListItemIcon>
+            </AdminListItemButton>
+          </MemberListItem>
+          <Divider />
+        </div>
+      ))}
+    </AdminContentList>
+  );
+};
+
+export default AdminMembersContent;
