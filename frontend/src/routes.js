4720a20aae7fde0aa49d520759aad53da4ccc793
Michael Vo <zax@zaxu.xyz>
2022-04-07 21:33:41 +1000

    [CHAOS-16] Add page for finalising candidates after ranking them (#68)
    * add finalise candidates page
    
    * add finalise candidates route
    
    * navigation between rankings and finalise
    
    * don't have a name for unfinalised roles
    
    * add tabs

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -7,6 +7,7 @@ const AuthSuccess = lazy(() => import("./pages/auth_success"));
 const SignupPage = lazy(() => import("./pages/signup"));
 const Marking = lazy(() => import("./pages/marking"));
 const Rankings = lazy(() => import("./pages/rankings"));
+const FinaliseCandidates = lazy(() => import("./pages/finalise_candidates"));
 const Admin = lazy(() => import("./pages/admin"));
 const CampaignCreate = lazy(() => import("./pages/create_campaign"));
 const ApplicationPage = lazy(() => import("./pages/application_page"));
@@ -18,6 +19,11 @@ const routes = [
   <Route key="landing" path="/" element={<LandingPage />} />,
   <Route key="marking" path="/marking" element={<Marking />} />,
   <Route key="rankings" path="/rankings" element={<Rankings />} />,
+  <Route
+    key="finalise-candidates"
+    path="/finalise_candidates"
+    element={<FinaliseCandidates />}
+  />,
   <Route key="Admin" path="/Admin" element={<Admin />} />,
   <Route
     key="create-campaign"
