ba9850d4e06f2ca999ac97dd8fe8d3547952cc29
Michael Vo <zax@zaxu.xyz>
2022-07-20 21:29:36 +1000

    [CHAOS-120] add people to org (#177)
    * add InputPopup component
    
    * add member invite popup
    
    * put InputPopup in a place that makes slightly more sense
    
    * feat(InputPopup): allow regular children
    
    * fix(AdminMembersContent): add keys
    
    * fix(InputPopup): transformOrigin right
    
    * feat(admin): integrate member invite with backend
    
    * fix(InputPopup): correct children prop type

--- frontend/src/pages/admin/AdminContent/AdminMembersContent.js
+++ frontend/src/pages/admin/AdminContent/AdminMembersContent.js
@@ -1,9 +1,21 @@
-import React from "react";
-import { ListItemText, Divider, ListItemIcon, IconButton } from "@mui/material";
+import React, { useState } from "react";
+import {
+  ListItemText,
+  Divider,
+  ListItemIcon,
+  IconButton,
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  Radio,
+  FormControlLabel,
+} from "@mui/material";
 import ClearIcon from "@mui/icons-material/Clear";
 import EditIcon from "@mui/icons-material/Edit";
 import AddIcon from "@mui/icons-material/Add";
 import PropTypes from "prop-types";
+import InputPopup from "components/InputPopup";
+import { inviteUserToOrg } from "api";
 import {
   AdminContentList,
   DummyIconForAlignment,
@@ -13,10 +25,14 @@ import {
   AdminListItemButton,
 } from "./adminContent.styled";
 
-const AdminMembersContent = ({ members, setMembers }) => {
+const AdminMembersContent = ({ orgId, members, setMembers }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
   const onDelete = (memberId) => {
     // FIXME: CHAOS-55, integrate with backend to actually delete
     setMembers(members.filter((m) => m.id !== parseInt(memberId, 10)));
+  };
+  const inviteUser = (formValues) => {
+    inviteUserToOrg(formValues.email, orgId, formValues.adminLevel);
   };
   return (
     <AdminContentList>
@@ -27,14 +43,47 @@ const AdminMembersContent = ({ members, setMembers }) => {
           <DummyIconForAlignment />
         </ListItemIcon>
         <ListItemIcon>
-          <IconButton>
+          <IconButton onClick={(e) => setAnchorEl(e.currentTarget)}>
             <AddIcon />
           </IconButton>
+          <InputPopup
+            title="Invite a user"
+            label="Email"
+            name="email"
+            submitText="Invite"
+            defaultState={{ adminLevel: "ReadOnly" }}
+            onSubmit={inviteUser}
+            open={Boolean(anchorEl)}
+            anchorEl={anchorEl}
+            setAnchorEl={setAnchorEl}
+          >
+            {({ formValues, handleInputChange }) => (
+              <FormControl>
+                <FormLabel id="admin-level">Admin Level</FormLabel>
+                <RadioGroup
+                  aria-labelledby="admin-level"
+                  value={formValues.adminLevel}
+                  onChange={handleInputChange}
+                  name="adminLevel"
+                  row
+                >
+                  {["Read Only", "Director", "Admin"].map((label) => (
+                    <FormControlLabel
+                      key={label}
+                      value={label.replace(" ", "")}
+                      control={<Radio size="small" />}
+                      label={label}
+                    />
+                  ))}
+                </RadioGroup>
+              </FormControl>
+            )}
+          </InputPopup>
         </ListItemIcon>
       </ContentListHeader>
       <AdminDivider />
       {members.map((m) => (
-        <div>
+        <div key={m.id}>
           <MemberListItem>
             <AdminListItemButton>
               <ListItemText align="center">{m.name}</ListItemText>
@@ -62,6 +111,7 @@ const AdminMembersContent = ({ members, setMembers }) => {
 };
 
 AdminMembersContent.propTypes = {
+  orgId: PropTypes.number.isRequired,
   members: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number,
