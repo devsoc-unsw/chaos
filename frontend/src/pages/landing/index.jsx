89c6bf4516aba489cc3207c708742f8ba69c9145
Michael Vo <zax@zaxu.xyz>
2022-09-06 13:51:08 +1000

    feat(frontend/landing): implement redesign

--- frontend/src/pages/landing/index.jsx
+++ frontend/src/pages/landing/index.jsx
@@ -1,40 +1,83 @@
-import { Button } from "@mui/material";
-import React, { useContext, useEffect } from "react";
-import { Box } from "@mui/system";
-import { useNavigate } from "react-router-dom";
+import React, { useContext, useState, useEffect, Fragment } from "react";
+import tw from "twin.macro";
+import { Link } from "react-router-dom";
 import { SetNavBarTitleContext } from "contexts/SetNavbarTitleContext";
-import { BackgroundWrapper, ParticleWallpaper } from "../../components";
-import { BoldTitle, Subtitle } from "./landing.styled";
 import { getStore } from "../../utils";
+import Campaigns from "./components/Campaigns";
+import DashboardButton from "./components/DashboardButton";
+import Waves from "./components/Waves";
+import { Transition } from "components";
+
+import chaosImg from "assets/chaos.png";
 
 const OAUTH_CALLBACK_URL =
   getStore("AUTH_TOKEN") || import.meta.env.VITE_OAUTH_CALLBACK_URL;
 
 const Landing = () => {
-  // eslint-disable-next-line no-unused-vars
-  const [loggedIn, setLoggedIn] = React.useState(false);
-  const navigate = useNavigate();
-
   const setNavBarTitle = useContext(SetNavBarTitleContext);
   useEffect(() => {
     setNavBarTitle("");
   }, []);
 
+  const [offsetX, setOffsetX] = useState(0);
+  const [offsetY, setOffsetY] = useState(0);
+  const onPointerMove = (e) => {
+    const div = e.currentTarget;
+    setOffsetX((e.clientX - (div.offsetLeft + div.clientWidth / 2)) / 100);
+    setOffsetY((e.clientY - (div.offsetTop + div.clientHeight / 2)) / 100);
+  };
+
   return (
-    <BackgroundWrapper>
-      <ParticleWallpaper />
-      <BoldTitle variant="h1">Project Chaos</BoldTitle>
-      <Subtitle variant="subtitle">
-        Recruitment Drives, without the fuss.
-      </Subtitle>
-      <Box>
-        {getStore("AUTH_TOKEN") ? (
-          <Button onClick={() => navigate("/dashboard")}>Your Dashboard</Button>
-        ) : (
-          <Button href={OAUTH_CALLBACK_URL}> Get Started </Button>
-        )}
-      </Box>
-    </BackgroundWrapper>
+    <div
+      tw="flex flex-col items-center min-h-screen w-full font-light font-sans bg-gray-50"
+      onPointerMove={onPointerMove}
+    >
+      <div tw="flex w-full p-[50px] max-w-7xl my-auto translate-y-[-100px]">
+        <main>
+          <Transition
+            appear={true}
+            enter={tw`transition duration-[600ms]`}
+            enterFrom={tw`opacity-0 -translate-x-6`}
+          >
+            <h1 tw="text-5xl">
+              <img
+                tw="inline h-[1em] filter drop-shadow-md"
+                src={chaosImg}
+                alt="Chaos Logo"
+              />{" "}
+              Chaos
+            </h1>
+          </Transition>
+          <Transition
+            as={Fragment}
+            appear={true}
+            enter={tw`transition duration-[600ms] delay-[250ms]`}
+            enterFrom={tw`opacity-0 translate-y-4`}
+            enterTo={tw`text-2xl`}
+          >
+            <h2 tw="text-3xl my-4">Recruitment Drives, without the fuss.</h2>
+          </Transition>
+          <Transition
+            appear={true}
+            enter={tw`transition duration-[600ms] delay-500`}
+            enterFrom={tw`opacity-0 translate-y-4`}
+          >
+            {getStore("AUTH_TOKEN") ? (
+              <DashboardButton as={Link} to="/dashboard">
+                Your Dashboard
+              </DashboardButton>
+            ) : (
+              <DashboardButton as="a" href={OAUTH_CALLBACK_URL}>
+                Get Started
+              </DashboardButton>
+            )}
+          </Transition>
+        </main>
+        <Campaigns offsetX={offsetX} offsetY={offsetY} />
+      </div>
+
+      <Waves />
+    </div>
   );
 };
 
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+const colors = require("tailwindcss/colors");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors,
+    extend: {},
+  },
+  plugins: [require("tailwindcss-gradient")],
+};
