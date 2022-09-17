a037f356920af0c4c341cfea39a1ea52a9946d4b
Michael Vo <zax@zaxu.xyz>
2022-09-17 15:26:16 +1000

    feat(frontend): use open sans

--- frontend/src/styles/globalStyles.js
+++ frontend/src/styles/globalStyles.js
@@ -2,7 +2,7 @@ import tw, { globalStyles } from "twin.macro";
 import { globalCss } from "../../stitches.config";
 
 const customStyles = {
-  body: tw`antialiased`,
+  body: tw`antialiased font-sans!`,
 };
 
 const styles = () => {
