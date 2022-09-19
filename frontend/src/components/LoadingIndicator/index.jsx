16fcf5b671cf2c236973bcb60fba556cd5f39838
Michael Vo <zax@zaxu.xyz>
2022-09-20 04:11:13 +1000

    fix(frontend): show loading indicator properly between routes (#203)
    * fix(frontend): only wrap route in Suspense
    
    * fix(frontend): immediately show loading indicator
    
    * fix(frontend): center loading text
    
    * refactor(LoadingIndicator): remove unnecessary styles

--- frontend/src/components/LoadingIndicator/index.jsx
+++ frontend/src/components/LoadingIndicator/index.jsx
@@ -1,23 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { LinearProgress, Typography } from "@mui/material";
+import { LinearProgress } from "@mui/material";
+import "twin.macro";
 
-const LoadingIndicator = () => {
-  const [show, setShow] = useState(false);
-  useEffect(() => {
-    const timeout = setTimeout(() => setShow(true), 400);
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, []);
-
-  return (
-    show && (
-      <>
-        <LinearProgress />
-        <Typography>Loading...</Typography>
-      </>
-    )
-  );
-};
+const LoadingIndicator = () => (
+  <div tw="flex flex-col flex-1">
+    <LinearProgress />
+    <div tw="flex-1 flex items-center justify-center">Loading...</div>
+  </div>
+);
 
 export default LoadingIndicator;
