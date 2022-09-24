e738ec0607d41fb19ba6e497ac416cffac8e132e
Michael Vo <zax@zaxu.xyz>
2022-09-25 03:44:26 +1000

    fix(contexts): allow empty functions

--- frontend/src/contexts/SetNavbarTitleContext.ts
+++ frontend/src/contexts/SetNavbarTitleContext.ts
@@ -1,3 +1,4 @@
 import { createContext } from "react";
 
+// eslint-disable-next-line @typescript-eslint/no-empty-function
 export const SetNavBarTitleContext = createContext((_title: string) => {});
