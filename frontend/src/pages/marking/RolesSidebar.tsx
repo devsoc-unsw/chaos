95e3fa9d4f51c0525abca083adc65580e31bec68
Michael Vo <zax@zaxu.xyz>
2022-10-23 16:45:46 +1100

    fix(review/sidebar): don't require matching .../marking

--- frontend/src/pages/marking/RolesSidebar.tsx
+++ frontend/src/pages/marking/RolesSidebar.tsx
@@ -1,9 +1,9 @@
-import { NavLink } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import tw, { styled } from "twin.macro";
 
 import type { Role } from "types/api";
 
-const RoleItem = styled(NavLink, {
+const RoleItem = styled(Link, {
   ...tw`block px-3 py-1.5 rounded w-full text-left font-normal`,
 
   ...tw`relative z-0 hover:before:opacity-20`,
@@ -16,23 +16,32 @@ const RoleItem = styled(NavLink, {
     `,
   },
 
-  "&.active": tw`shadow-sm before:opacity-30!`,
+  variants: {
+    active: {
+      true: tw`shadow-sm before:opacity-30!`,
+    },
+  },
 });
 
 type Props = {
   roles: Role[];
 };
-const RolesSidebar = ({ roles }: Props) => (
-  <nav tw="absolute bottom-0 left-0 p-4 bg-white shadow-xl top-16 w-80">
-    <h2 tw="mb-4 text-2xl">Roles</h2>
-    <ul tw="flex flex-col gap-1">
-      {roles.map((role) => (
-        <li key={role.id}>
-          <RoleItem to={`${role.id}/marking`}>{role.name}</RoleItem>
-        </li>
-      ))}
-    </ul>
-  </nav>
-);
+const RolesSidebar = ({ roles }: Props) => {
+  const roleId = Number(useParams().roleId);
+  return (
+    <nav tw="absolute bottom-0 left-0 p-4 bg-white shadow-xl top-16 w-80">
+      <h2 tw="mb-4 text-2xl">Roles</h2>
+      <ul tw="flex flex-col gap-1">
+        {roles.map((role) => (
+          <li key={role.id}>
+            <RoleItem to={`${role.id}/marking`} active={role.id === roleId}>
+              {role.name}
+            </RoleItem>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
 export default RolesSidebar;
