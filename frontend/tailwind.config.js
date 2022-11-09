bf72743ab2634b5c8641d7bf8412bd08a760f313
Michael Vo <zax@zaxu.xyz>
2022-11-09 17:21:35 +1100

    chore(tailwind): don't replace colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,19 +1,14 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
-const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors,
     extend: {
       fontFamily: {
         sans: ["Lato", ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  plugins: [
-    require("tailwindcss-gradient"),
-    require('@tailwindcss/forms'),
-  ],
+  plugins: [require("tailwindcss-gradient"), require("@tailwindcss/forms")],
 };
