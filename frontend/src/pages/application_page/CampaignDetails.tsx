4993ecc6426796fda7403d9a72f0e4d708964fd8
Michael Vo <zax@zaxu.xyz>
2022-11-09 13:49:46 +1100

    fix(application_page): anchor cover image to right

--- frontend/src/pages/application_page/CampaignDetails.tsx
+++ frontend/src/pages/application_page/CampaignDetails.tsx
@@ -32,7 +32,7 @@ const CampaignDetails = ({
   userInfo,
 }: Props) => (
   <Card as="header" tw="items-center gap-6 md:(flex-row items-start)">
-    <article tw="flex flex-col gap-2">
+    <article tw="flex flex-col w-full gap-2">
       <div tw="flex items-center gap-2">
         <img
           tw="h-20 rounded shadow-md"
@@ -54,19 +54,19 @@ const CampaignDetails = ({
         <h3 tw="text-xl leading-loose">You&apos;re applying as:</h3>
         <p tw="flex gap-1.5">
           <span>{userInfo.display_name}</span>
-          <span tw="font-light italic">({userInfo.zid})</span>
+          <span tw="italic font-light">({userInfo.zid})</span>
           <span>·</span>
           <span>{userInfo.degree_name}</span>
         </p>
-        <p tw="flex items-center gap-1 text-gray-800 text-sm">
-          <EnvelopeIcon tw="h-4 w-4" /> {userInfo.email}
+        <p tw="flex items-center text-sm text-gray-800 gap-1">
+          <EnvelopeIcon tw="w-4 h-4" /> {userInfo.email}
         </p>
       </div>
     </article>
 
-    <aside tw="m-auto flex max-w-lg flex-shrink-0 items-center justify-center overflow-hidden rounded shadow-md bg-[#edeeef] aspect-ratio[16/9] md:w-1/2">
+    <aside tw="m-auto w-full flex max-w-lg flex-shrink-0 items-center justify-center overflow-hidden rounded shadow-md bg-[#edeeef] aspect-ratio[16/9] md:(w-1/2 mr-0)">
       <img
-        tw="max-h-full w-full object-contain"
+        tw="object-contain w-full max-h-full"
         src={headerImage}
         alt={campaignName}
       />
