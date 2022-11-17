6dabfcfda0b2dc3acd962c1b1b8da767d2513a9f
Michael Vo <zax@zaxu.xyz>
2022-11-17 23:36:10 +1100

    feat(CampaignCard): add button shadow

--- frontend/src/components/CampaignCard/CampaignStatus.tsx
+++ frontend/src/components/CampaignCard/CampaignStatus.tsx
@@ -1,29 +1,15 @@
-import { ChevronUpDownIcon } from "@heroicons/react/20/solid";
 import tw, { styled } from "twin.macro";
 
-import type { ComponentProps, PropsWithChildren } from "react";
-
-const StatusIndicator = ({
-  children,
-  ...props
-}: PropsWithChildren<ComponentProps<"button">>) => (
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  <button type="button" {...props}>
-    <ChevronUpDownIcon tw="-mx-0.5 h-4 w-4" />
-    {children}
-  </button>
-);
-
-const CampaignStatus = styled(StatusIndicator, {
-  ...tw`ml-auto flex items-center gap-1 px-2 py-1.5 text-white rounded-[0.2rem]`,
+const CampaignStatus = styled.button({
+  ...tw`ml-auto flex items-center gap-1 px-2 py-1.5 text-white rounded-[0.2rem] shadow transition hover:shadow-md`,
 
   variants: {
     status: {
-      pending: tw`text-black bg-[hsl(220, 60%, 90%)]`,
-      open: tw`bg-[hsl(220, 93%, 60%)]`,
+      pending: tw`text-black bg-[hsl(220, 60%, 90%)] shadow-[hsl(220, 60%, 90%)]!`,
+      open: tw`bg-[hsl(220, 93%, 60%)] shadow-[hsla(220, 93%, 60%, 50%)]!`,
       closed: tw`bg-gray-100 text-black`,
-      offered: tw`bg-green-200 text-green-900`,
-      rejected: tw`bg-red-200 text-red-900`,
+      offered: tw`bg-green-200 text-green-900 shadow-green-200! hover:bg-green-300`,
+      rejected: tw`bg-red-200 text-red-900 shadow-red-200!`,
     },
   },
 
