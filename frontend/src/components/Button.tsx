3a8e602d637d8d213f49d876cc85b73f5efb42c9
Michael Vo <zax@zaxu.xyz>
2022-11-21 23:01:11 +1100

    feat(button): hover+focus styles

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,12 +1,15 @@
 import tw, { styled } from "twin.macro";
 
 const Button = styled.button({
-  ...tw`px-3 py-1.5 rounded shadow transition disabled:(opacity-50 cursor-not-allowed)`,
+  ...tw`
+    px-3 py-1.5 rounded shadow transition disabled:(opacity-50 cursor-not-allowed)
+    outline-none focus:ring
+  `,
 
   variants: {
     color: {
-      primary: tw`bg-[hsl(220, 93%, 60%)] text-white`,
-      danger: tw`bg-red-600 text-white hover:bg-red-700`,
+      primary: tw`bg-[hsl(220, 93%, 60%)] text-white hocus:bg-[hsl(220, 93%, 54%)]`,
+      danger: tw`bg-red-600 text-white hocus:bg-red-700`,
     },
   },
 
