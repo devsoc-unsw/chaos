c67ccbfe8098e60042e9a20d7f654e0970fde4ac
Michael Vo <zax@zaxu.xyz>
2022-11-23 01:07:54 +1100

    feat(button): increase outlined hover style text darkness

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -10,7 +10,7 @@ const Button = styled.button({
     color: {
       primary: tw`bg-brand-500 text-white hocus:bg-brand-600`,
       danger: tw`bg-red-600 text-white hocus:bg-red-700`,
-      white: tw`bg-white border border-brand-500 text-gray-900 hocus:(bg-brand-50 text-brand-900)`,
+      white: tw`bg-white border border-brand-300 text-gray-900 hocus:(bg-brand-50 text-brand-950 border-brand-400)`,
     },
   },
 
