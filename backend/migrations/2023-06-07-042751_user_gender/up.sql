ace96a40e7ddbf8ce4bcbe829fdced5025c214f6
William Zhang <williamzhang1478@gmail.com>
2023-06-14 14:43:10 +1000

    fix: removed 'Other' gender

--- backend/migrations/2023-06-07-042751_user_gender/up.sql
+++ backend/migrations/2023-06-07-042751_user_gender/up.sql
@@ -1,4 +1,4 @@
-CREATE TYPE user_gender AS ENUM ('Female', 'Male', 'Other', 'Unspecified');
+CREATE TYPE user_gender AS ENUM ('Female', 'Male', 'Unspecified');
 
 ALTER TABLE users
     ADD COLUMN gender user_gender DEFAULT 'Unspecified';
