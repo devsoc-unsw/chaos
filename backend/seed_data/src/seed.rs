b029e7d809d09b688ee85e6ba17de930a380f1b6
William Zhang <williamzhang1478@gmail.com>
2023-09-21 01:39:30 +1000

    fix: removed organisation vector debug print

--- backend/seed_data/src/seed.rs
+++ backend/seed_data/src/seed.rs
@@ -133,9 +133,7 @@ pub fn seed() {
             .expect(&format!("Failed to insert org {}.", org.name));
     }
 
-    let x = Organisation::get_all(&connection);
-    println!("{:?}", x);
-    assert!(x.len() == 2);
+    assert!(Organisation::get_all(&connection).len() == 2);
 
     println!("... Added {} organizations\n", orgs.len());
     // make giuliana the admin of csesoc
