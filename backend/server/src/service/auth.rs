03a487777a6294a9df1a50dfecba48d5d7d0acd5
Kavika <kavika@csesoc.org.au>
2024-02-11 16:51:48 +1100

    OAuth flow account creation/login (#460)
    * start implementing authorisation
    
    * feat(backend): implement extractor for AuthUser (user id)
    
    * feat(backend): implement extractor for SuperUser (user id + authZ)
    
    * feat(backend): get existing or create new user for Google login
    
    * feat(backend): get name from Google user profile
    
    * feat(backend): update schema.prisma to include lowercase table names + updated_at default time for users
    
    ---------
    
    Co-authored-by: kappamalone <uzman.zawahir1@gmail.com>

--- backend/server/src/service/auth.rs
+++ backend/server/src/service/auth.rs
@@ -1,21 +1,41 @@
 use anyhow::Result;
 use jsonwebtoken::{DecodingKey, EncodingKey};
+use snowflake::SnowflakeIdGenerator;
 use sqlx::{Pool, Postgres};
+use crate::models::user::UserRole;
 
 /// Checks if a user exists in DB based on given email address. If so, their user_id is returned.
 /// Otherwise, a new user is created in the DB, and the new id is returned.
 /// This function is used in OAuth flows to login/signup users when they click the
 /// "Sign in with ___" buttons. The returned user_id will be used to generate a JWT to be
 /// used as a token for the user's browser.
-pub async fn create_or_get_user_id(email: String, pool: Pool<Postgres>) -> Result<i64> {
-    // TODO: See if user (by email) exists in the database and return their id. If not, insert them, and return the new id.
+pub async fn create_or_get_user_id(email: String, name: String, pool: Pool<Postgres>, mut snowflake_generator: SnowflakeIdGenerator) -> Result<i64> {
+    let possible_user_id = sqlx::query!("SELECT id FROM users WHERE email = $1", email)
+        .fetch_optional(&pool)
+        .await?;
 
-    let user_id = 1;
+    if let Some(result) = possible_user_id {
+        return Ok(result.id);
+    }
+
+    let user_id = snowflake_generator.real_time_generate();
+
+    let response = sqlx::query!(
+            "INSERT INTO users (id, email, name) VALUES ($1, $2, $3)",
+            user_id, email, name
+        )
+        .execute(&pool)
+        .await?;
+
     return Ok(user_id);
 }
 
 pub async fn is_super_user(user_id: i64, pool: &Pool<Postgres>) -> Result<bool> {
-    let is_super_user = sqlx::query!("SELECT EXISTS(SELECT 1 FROM users WHERE id = $1 AND role = $2)", user_id, UserRole::SuperUser)
+    let is_super_user = sqlx::query!(
+            "SELECT EXISTS(SELECT 1 FROM users WHERE id = $1 AND role = $2)",
+            user_id,
+            UserRole::SuperUser as UserRole
+        )
         .fetch_one(pool)
         .await?;
 
