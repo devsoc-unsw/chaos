4cb20410bedcb7ec4ace0f4279c72b3dc6a9a890
Kavika <kbpalletenne@gmail.com>
2024-04-06 14:36:04 +1100

    feat(backend): create sql migrations

---
+++ backend/migrations/20240406023149_create_users.sql
@@ -0,0 +1,13 @@
+CREATE TYPE user_role AS ENUM ('User', 'SuperUser');
+
+CREATE TABLE users (
+    id BIGINT PRIMARY KEY,
+    email TEXT NOT NULL UNIQUE,
+    zid TEXT,
+    name TEXT NOT NULL,
+    degree_name TEXT,
+    degree_starting_year INTEGER,
+    role user_role NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
