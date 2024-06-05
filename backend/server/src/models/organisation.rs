f61e1df62151bfbbe6ecf1f8a0a21123491d4048
Alexander <alexander.l.lai@student.unsw.edu.au>
2024-06-05 21:00:26 +1000

    implement feedback

--- backend/server/src/models/organisation.rs
+++ backend/server/src/models/organisation.rs
@@ -10,11 +10,13 @@ pub struct Organisation {
     pub created_at: DateTime<Utc>,
     pub updated_at: DateTime<Utc>,
     pub campaigns: Vec<Campaign>,                       // Awaiting Campaign to be complete - remove comment once done
-    pub organisation_admins: Vec<OrganisationAdmins>
+    pub organisation_admins: Vec<i64>
 }
 
-#[derive(Deserialize, Serialize, Clone, Debug)]
-pub struct OrganisationAdmins {
-    pub organisation_id: i64,    // References id in Organisation table
-    pub user_id: i64             // References id in User table
+#[derive(Deserialize, Serialize)]
+pub struct OrganisationDetails {
+    pub id: i64,
+    pub name: String,
+    pub logo: Option<String>,
+    pub created_at: DateTime<Utc>
 }
--- backend/server/src/service/organisation.rs
+++ backend/server/src/service/organisation.rs
@@ -1,15 +1,9 @@
-use anyhow::{bail, Result};
+use anyhow::{bail, Error, Result};
 use sqlx::{Pool, Postgres};
 use chrono::{DateTime, Local, NaiveDateTime, Utc};
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize)]
-pub struct OrganisationDetails {
-    pub id: i64,
-    pub name: String,
-    pub logo: Option<String>,
-    pub created_at: DateTime<Utc>
-}
+use crate::models::organisation::OrganisationDetails;
 
 pub async fn get_organisation(id: i64, pool: Pool<Postgres>) -> Result<OrganisationDetails> {
     let organisation_details = sqlx::query!
@@ -36,112 +30,63 @@ pub async fn get_organisation(id: i64, pool: Pool<Postgres>) -> Result<Organisat
 }
 
 
-pub async fn update_organisation_logo(id: i64, logo: String, pool: Pool<Postgres>) -> Result<String> {
+pub async fn update_organisation_logo(id: i64, logo: String, pool: Pool<Postgres>) -> Result<()> {
     let dt = Local::now();
     let current_time = dt.naive_utc();
-    let updated_organisation = sqlx::query!
+    sqlx::query!
     (
         "
             UPDATE organisations
                 SET logo = $2, updated_at = $3
                 WHERE id = $1
-                RETURNING *
         ",
         id,
         logo,
         current_time
     ).fetch_one(&pool)
-    .await;
+    .await?;
 
-    match updated_organisation {
-        Ok(_) => Ok(logo),
-        Err(_) => bail!("error: Failed to update organisation logo")
-    }
+    Ok(())
 }
 
-pub async fn delete_organisation(organisation_id: i64, admin_id: i64, pool: Pool<Postgres>) -> Result<String, String> {
-   // Delete all organisation_admins with the corresponding organisation_id
-   let deleted_users = sqlx::query!
-    (
-        "
-            WITH user_exists AS (
-                SELECT 1 AS exists_flag
-                FROM organisation_admins
-                WHERE organisation_id = $1
-                AND user_id = $2
-                LIMIT 1
-            )
-            DELETE FROM organisation_admins
-                WHERE organisation_id = $1
-                AND EXISTS (
-                    SELECT 1
-                    FROM user_exists
-                )
-                RETURNING *
-        ",
-        organisation_id,
-        admin_id
-   ).fetch_one(&pool)
-   .await;
-
-   match deleted_users {
-        Ok(_) => {},
-        Err(_) => return Err("Unauthorized".to_string())
-   }
-
-   
+pub async fn delete_organisation(organisation_id: i64, admin_id: i64, pool: Pool<Postgres>) -> Result<()> {
    // Delete organisation
-   let deleted_organisation = sqlx::query!
+   sqlx::query!
    (
         "
-            DELETE FROM organisations WHERE id = $1
+            CASCADE DELETE FROM organisations WHERE id = $1
                 RETURNING *
         ",
         organisation_id
     ).fetch_one(&pool)
-    .await;
+    .await?;
 
-  match deleted_organisation {
-    Ok(_) => {
-        Ok("Successfully deleted organisation.".to_string())
-    },
-    Err(_) => Err("Unauthorized".to_string())
+    Ok(())
 }
 
-}
-
-pub async fn create_organisation(id: i64, name: String, pool: Pool<Postgres>) -> Result<String> {
-    let new_organisation = sqlx::query!
+pub async fn create_organisation(id: i64, name: String, pool: Pool<Postgres>) -> Result<(), Error> {
+    sqlx::query!
     (
         "
             INSERT INTO organisations (id, name)
                 VALUES ($1, $2)
-                RETURNING *
         ",
         id,
         name
-    ).fetch_one(&pool).await;
-
-    match new_organisation {
-        Ok(_) => {
-            Ok("Successfully created organisation.".to_string())
-        },
-        Err(_) => bail!("error: failed to create organisation")
-    }
+    ).fetch_one(&pool)
+    .await?;
+    
+    Ok(())
 }
 
-#[derive(Deserialize, Serialize)]
-pub struct Member {
-    pub id: i64,
-    pub name: String
-}
+// Below returns subset of user - not completed yet - remove comment when done
 pub async fn get_organisation_admins(organisation_id: i64, pool: Pool<Postgres>) -> Result<Vec<Member>> {
     let admin_list = sqlx::query!
     (
         "
-            SELECT user_id
-                FROM organisation_admins
-                WHERE organisation_id = $1
+            SELECT organisation_admins
+                FROM organisations
+                WHERE id = $1
         "
         , organisation_id
     ).fetch_all(&pool).await?;
@@ -155,75 +100,57 @@ pub async fn get_organisation_admins(organisation_id: i64, pool: Pool<Postgres>)
     let mut users = Vec::new();
 
     for id in admin_id_list {
-        let user_object = sqlx::query!(
-            "
-                SELECT id, name
-                FROM users
-                WHERE id = $1
-            "
-            , id
-            
-        )
-        .fetch_one(&pool)
-        .await?;
         users.push(
-            Member {
-                id: user_object.id,
-                name: user_object.name
-            }
+            sqlx::query_as!(
+                Member,
+                "
+                    SELECT id, name
+                    FROM users
+                    WHERE id = $1
+                "
+                , id
+            )
+            .fetch_one(&pool)
+            .await?
         )
     }
 
     Ok(users)
 }
 
-pub async fn update_organisation_admins(organisation_id: i64, admin_id_list: Vec<i64>, pool: Pool<Postgres>) -> Result<String> {
+pub async fn update_organisation_admins(organisation_id: i64, admin_id_list: Vec<i64>, pool: Pool<Postgres>) -> Result<(), Error> {
     sqlx::query!(
-        "DELETE FROM organisation_admins WHERE organisation_id = $1",
+        "
+            UPDATE organisations
+            SET organisation_admins = $1
+            WHERE id = $2
+        ",
+        &admin_ids,
         organisation_id
     )
-    .execute(&pool)
+    .execute(pool)
     .await?;
-
-    for admin_id in admin_id_list {
-        sqlx::query!(
-            "
-            INSERT INTO organisation_admins (organisation_id, user_id)
-            VALUES ($1, $2)
-            ",
-            organisation_id,
-            admin_id
-        )
-        .execute(&pool)
-        .await?;
-    }
-
-    Ok("Successfully updated members.".to_string())
+    
+    Ok(())
 }
 
-pub async fn remove_admin_from_organisation(organisation_id: i64, admin_id: i64, pool: Pool<Postgres>) -> Result<String> {
-    let new_organisation_admin = sqlx::query!
-    (
+pub async fn remove_admin_from_organisation(organisation_id: i64, admin_id: i64, pool: Pool<Postgres>) -> Result<(), Error> {
+    sqlx::query!(
         "
-            DELETE FROM organisation_admins
-                WHERE organisation_id = $1
-                AND user_id = $2
-                RETURNING *
+            UPDATE organisations
+            SET organisation_admins = array_remove(organisation_admins, $1)
+            WHERE id = $2
         ",
-        organisation_id,
-        admin_id
-    ).fetch_one(&pool).await;
-
-    match new_organisation_admin {
-        Ok(_) => {
-            println!("{:?}", new_organisation_admin);
-            Ok("Deleted organisation admin in database".to_string())
-        },
-        Err(_) => bail!("error: failed to remove admin from organisation")
-    }
+        admin_id,
+        organisation_id
+    )
+    .execute(pool)
+    .await?;
+    
+    Ok(())
 }
 
-
+// This is only here as a placeholder - replace when campaign is done
 #[derive(Deserialize, Serialize)]
 pub struct Campaign {
     // Define your struct fields based on the Campaign model
@@ -232,42 +159,38 @@ pub struct Campaign {
     pub name: String,
     pub cover_image: Option<String>,
     pub description: String,
-    pub starts_at: DateTime<Utc>,
-    pub ends_at: DateTime<Utc>,
+    pub starts_at: NaiveDateTime,
+    pub ends_at: NaiveDateTime
 }
+
+impl Campaign {
+    fn into_utc(self) -> Self {
+        Self {
+            starts_at: DateTime::from_utc(self.starts_at, Utc),
+            ends_at: DateTime::from_utc(self.ends_at, Utc),
+            ..self
+        }
+    }
+}
+
 pub async fn get_organisation_campaigns(id: i64, pool: Pool<Postgres>) -> Result<Vec<Campaign>> {
-    let campaigns = sqlx::query!
-    (
+    let campaigns = sqlx::query_as!(
+        Campaign,
         "
-            SELECT *
-                FROM campaigns
-                WHERE organisation_id = $1
+            SELECT id, name, cover_image, description, starts_at, ends_at
+            FROM campaigns
+            WHERE organisation_id = $1
         ",
         id
-    ).fetch_all(&pool).await;
+    )
+    .fetch_all(&pool)
+    .await?;
 
-    let mut result: Vec<Campaign> = Vec::new();
-    match campaigns {
-        Ok(campaign_list) => {
-            for campaign in campaign_list {
-                let details = Campaign {
-                    id: campaign.id,
-                    name: campaign.name,
-                    cover_image: campaign.cover_image,
-                    description: campaign.description,
-                    starts_at: campaign.starts_at.and_utc(),
-                    ends_at: campaign.ends_at.and_utc()
-                };
-                result.push(details);
-            }
-        },
-        Err(_) => bail!("Error getting organisation campaign")
-    }
-    Ok(result)
+    Ok(campaigns)
 }
 
-pub async fn create_campaign_for_organisation(id: i64, name: String, description: String, starts_at: NaiveDateTime, ends_at: NaiveDateTime, pool: Pool<Postgres>) -> Result<Campaign> {
-    let new_campaign = sqlx::query!
+pub async fn create_campaign_for_organisation(id: i64, name: String, description: String, starts_at: NaiveDateTime, ends_at: NaiveDateTime, pool: Pool<Postgres>) -> Result<(), Error> {
+    sqlx::query!
     (
         "
             INSERT INTO campaigns (id, name, description, starts_at, ends_at)
@@ -279,21 +202,7 @@ pub async fn create_campaign_for_organisation(id: i64, name: String, description
         description,
         starts_at,
         ends_at
-    ).fetch_one(&pool).await;
+    ).fetch_one(&pool).await?;
 
-    match new_campaign {
-        Ok(res) => {
-            Ok(
-                Campaign {
-                    id: res.id,
-                    name: res.name,
-                    cover_image: None,
-                    description: res.description,
-                    starts_at: res.starts_at.and_utc(),
-                    ends_at: res.ends_at.and_utc()
-                }
-            )
-        },
-        Err(_) => bail!("error: failed to create campaign")
-    }
+    Ok(())
 }
