4ae417febdb0644c9d8781d98ee6ec07c7e63039
Josh <joshconstantinelim@gmail.com>
2024-06-16 20:07:15 +1000

    set prettier config options

--- frontend/prettier.config.js
+++ frontend/prettier.config.js
@@ -1,4 +1,19 @@
+/**
+ * @type {import("prettier").Config}
+ */
 module.exports = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  printWidth: 80,
+  useTabs: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+
   plugins: ["prettier-plugin-tailwindcss"],
   tailwindAttributes: ["className", "class", "tw"],
 };
