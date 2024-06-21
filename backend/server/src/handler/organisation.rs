fdebc28f12caee0e2bbce9ebf6458531e945e8d0
Kavika <kbpalletenne@gmail.com>
2024-06-21 18:31:39 +0530

    simplify handlers to use new error type

--- backend/server/src/handler/organisation.rs
+++ backend/server/src/handler/organisation.rs
@@ -21,53 +21,42 @@ pub async fn update_organisation_logo(
     State(state): State<AppState>,
     Path(organisation_id): Path<i64>,
     user: AuthUser,
-) -> Result<impl IntoResponse, impl IntoResponse> {
-    match service::organisation::update_organisation_logo(organisation_id, user.user_id, &state.db)
-        .await
-    {
-        Ok(logo_url) => Ok((StatusCode::OK, Json(logo_url))),
-        Err(e) => Err((StatusCode::INTERNAL_SERVER_ERROR, e.to_string())),
-    }
+) -> Result<impl IntoResponse, ChaosError> {
+    let logo_url = service::organisation::update_organisation_logo(organisation_id, user.user_id, &state.db).await?;
+    Ok((StatusCode::OK, Json(logo_url)))
 }
 
 pub async fn delete_organisation(
     State(state): State<AppState>,
     Path(organisation_id): Path<i64>,
     _user: SuperUser,
-) -> Result<impl IntoResponse, impl IntoResponse> {
-    match service::organisation::delete_organisation(organisation_id, &state.db).await {
-        Ok(organisation) => Ok((StatusCode::OK, Json(organisation))),
-        Err(e) => return Err((StatusCode::UNAUTHORIZED, e.to_string())),
-    }
+) -> Result<impl IntoResponse, ChaosError> {
+    service::organisation::delete_organisation(organisation_id, &state.db).await?;
+    Ok((StatusCode::OK, "Successfully deleted organisation"))
 }
 
 pub async fn create_organisation(
     State(state): State<AppState>,
     _user: SuperUser,
     Json(data): Json<NewOrganisation>,
-) -> Result<impl IntoResponse, impl IntoResponse> {
-    match service::organisation::create_organisation(
+) -> Result<impl IntoResponse, ChaosError> {
+    service::organisation::create_organisation(
         data.admin,
         data.name,
         state.snowflake_generator,
         &state.db,
     )
-    .await
-    {
-        Ok(message) => Ok((StatusCode::OK, Json(message))),
-        Err(e) => return Err((StatusCode::NOT_FOUND, e.to_string())),
-    }
+    .await?;
+    Ok((StatusCode::OK, "Successfully created organisation"))
 }
 
-pub async fn get_organisation_admins(
+pub async fn get_organisation_members(
     State(state): State<AppState>,
     Path(organisation_id): Path<i64>,
     user: AuthUser,
-) -> Result<impl IntoResponse, impl IntoResponse> {
-    match service::organisation::get_organisation_members(organisation_id, user.user_id, &state.db).await {
-        Ok(organisation_admins) => Ok((StatusCode::OK, Json(organisation_admins))),
-        Err(e) => return Err((StatusCode::NOT_FOUND, e.to_string())),
-    }
+) -> Result<impl IntoResponse, ChaosError> {
+    let members = service::organisation::get_organisation_members(organisation_id, user.user_id, &state.db).await?;
+    Ok((StatusCode::OK, Json(members)))
 }
 
 pub async fn update_organisation_admins(
@@ -75,18 +64,16 @@ pub async fn update_organisation_admins(
     Path(organisation_id): Path<i64>,
     Json(request_body): Json<AdminUpdateList>,
     user: SuperUser,
-) -> Result<impl IntoResponse, impl IntoResponse> {
-    match service::organisation::update_organisation_admins(
+) -> Result<impl IntoResponse, ChaosError> {
+    service::organisation::update_organisation_admins(
         organisation_id,
         user.user_id,
         request_body.members,
         &state.db,
     )
-    .await
-    {
-        Ok(organisation_admins) => Ok((StatusCode::OK, Json(organisation_admins))),
-        Err(e) => return Err((StatusCode::NOT_FOUND, e.to_string())),
-    }
+    .await?;
+
+    Ok((StatusCode::OK, "Successfully updated organisation admins"))
 }
 
 pub async fn remove_admin_from_organisation(
@@ -94,41 +81,36 @@ pub async fn remove_admin_from_organisation(
     Path(organisation_id): Path<i64>,
     user: AuthUser,
     Json(request_body): Json<AdminToRemove>,
-) -> Result<impl IntoResponse, impl IntoResponse> {
-    match service::organisation::remove_admin_from_organisation(
+) -> Result<impl IntoResponse, ChaosError> {
+    service::organisation::remove_admin_from_organisation(
         organisation_id,
         user.user_id,
         request_body.user_id,
         &state.db,
     )
-    .await
-    {
-        Ok(organisation_admins) => Ok((StatusCode::OK, Json(organisation_admins))),
-        Err(e) => return Err((StatusCode::NOT_FOUND, e.to_string())),
-    }
+    .await?;
+
+    Ok((StatusCode::OK, "Successfully removed admin from organisation"))
 }
 
 pub async fn get_organisation_campaigns(
     State(state): State<AppState>,
     Path(organisation_id): Path<i64>,
-) -> Result<impl IntoResponse, impl IntoResponse> {
-    match service::organisation::get_organisation_campaigns(organisation_id, &state.db).await {
-        Ok(organisation_admins) => Ok((StatusCode::OK, Json(organisation_admins))),
-        Err(e) => {
-            return Err((StatusCode::NOT_FOUND, e.to_string()))
-        },
-    }
+) -> Result<impl IntoResponse, ChaosError> {
+    let campaigns = service::organisation::get_organisation_campaigns(organisation_id, &state.db).await?;
+
+    Ok((StatusCode::OK, Json(campaigns)))
 }
 
 pub async fn create_campaign_for_organisation(
     State(state): State<AppState>,
     _user: SuperUser,
     Json(request_body): Json<models::campaign::Campaign>,
-) -> Result<impl IntoResponse, impl IntoResponse> {
+) -> Result<impl IntoResponse, ChaosError> {
     let mut snowflake_generator = state.snowflake_generator;
     let new_campaign_id = snowflake_generator.real_time_generate();
 
-    match service::organisation::create_campaign_for_organisation(
+    service::organisation::create_campaign_for_organisation(
         new_campaign_id,
         request_body.name,
         request_body.description,
@@ -136,9 +118,7 @@ pub async fn create_campaign_for_organisation(
         request_body.ends_at,
         &state.db,
     )
-    .await
-    {
-        Ok(message) => Ok((StatusCode::OK, Json(message))),
-        Err(e) => return Err((StatusCode::NOT_FOUND, e.to_string())),
-    }
+    .await?;
+
+    Ok((StatusCode::OK, "Successfully created campaign"))
 }
