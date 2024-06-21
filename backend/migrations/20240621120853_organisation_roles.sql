7468b161731d1993dbc6261d65567d411b05c4b7
Kavika <kbpalletenne@gmail.com>
2024-06-21 18:12:05 +0530

    update migration timestamps to be `NOT NULL`

--- backend/migrations/20240621120853_organisation_roles.sql
+++
@@ -1,4 +0,0 @@
-CREATE TYPE organisation_role AS ENUM ('User', 'Admin');
-
-ALTER TABLE organisation_admins RENAME TO organisation_members;
-ALTER TABLE organisation_members ADD COLUMN role organisation_role DEFAULT 'User' NOT NULL;
