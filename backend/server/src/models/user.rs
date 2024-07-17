45bd690f9de49e75b8ef47be49f53ae86f2acce8
Kavika <kbpalletenne@gmail.com>
2024-07-17 15:02:01 +1000

    set optional fields to `Option` in `User` struct

--- backend/server/src/models/user.rs
+++ backend/server/src/models/user.rs
@@ -1,20 +1,22 @@
 use serde::{Deserialize, Serialize};
+use sqlx::FromRow;
 
 #[derive(Deserialize, Serialize, sqlx::Type, Clone)]
-#[sqlx(type_name = "UserRole", rename_all = "PascalCase")]
+#[sqlx(type_name = "user_role", rename_all = "PascalCase")]
 pub enum UserRole {
     User,
     SuperUser,
 }
 
+#[derive(Deserialize, Serialize, FromRow)]
 pub struct User {
     pub id: i64,
     pub email: String,
-    pub zid: String,
+    pub zid: Option<String>,
     pub name: String,
-    pub degree_name: String,
-    pub degree_starting_year: i64,
-    pub role: String
+    pub degree_name: Option<String>,
+    pub degree_starting_year: Option<i32>,
+    pub role: UserRole
 }
 
 pub struct UserName {
