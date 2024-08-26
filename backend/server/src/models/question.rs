2a6f28f18511239256e75df8d5a3c779bc785315
Kavika <kbpalletenne@gmail.com>
2024-08-26 19:05:23 +0530

    update `Question` documentation to fit `serde` representation

--- backend/server/src/models/question.rs
+++ backend/server/src/models/question.rs
@@ -4,9 +4,28 @@ use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use sqlx::{Pool, Postgres, QueryBuilder, Row};
 
+/// The `Question` type that will be sent in API responses.
+///
+///
+/// With the chosen `serde` representation and the use of `#[serde(flatten)]`, the JSON for a
+/// `Question` will look like this:
+/// ```json
+/// {
+///   "id": "7233828375289139200",
+///   "title": "What is your favourite language?",
+///   "required": true,
+///   "question_type": "MultiChoice",
+///   "data": {
+///     "options": ["Rust", "Java", "TypeScript"]
+///   },
+///   "campaign_id": "7233828393345617920",
+///   "created_at": "2024-06-28T16:29:04.644008111Z",
+///   "updated_at": "2024-06-30T12:14:12.458390190Z"
+/// }
+/// ```
 #[derive(Deserialize, Serialize)]
 pub struct Question {
-    id: i64,
+    id: String,
     title: String,
     description: Option<String>,
     required: bool,
@@ -14,7 +33,7 @@ pub struct Question {
     #[serde(flatten)]
     question_type: QuestionType,
 
-    campaign_id: i64,
+    campaign_id: String,
     created_at: DateTime<Utc>,
     updated_at: DateTime<Utc>,
 }
@@ -26,22 +45,6 @@ pub struct Question {
 /// Some question types are stored in memory and JSON using the same struct, and only differ
 /// in their implementation when inserting to the database and in their restrictions
 /// (e.g. max 1 answer allowed in multi-choice vs. many in multi-select)
-///
-/// \
-/// With the chosen serde representation and the use of `#[serde(flatten)]`, the JSON for a
-/// `Question` will look like this:
-/// ```json
-/// {
-///   "title": "What is your favourite language?",
-///   "required": true,
-///   "question_type": "MultiChoice",
-///   "data": [
-///     "Rust",
-///     "Java",
-///     "TypeScript"
-///   ]
-/// }
-/// ```
 #[derive(Deserialize, Serialize)]
 #[serde(tag = "question_type", content = "data")]
 pub enum QuestionType {
