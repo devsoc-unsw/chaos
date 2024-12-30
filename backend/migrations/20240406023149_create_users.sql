877c388328eb692a61b739d1302812ab3bbdec72
Kavika <kbpalletenne@gmail.com>
2024-12-30 19:36:08 +0530

    optional user pronouns and gender

--- backend/migrations/20240406023149_create_users.sql
+++ backend/migrations/20240406023149_create_users.sql
@@ -5,11 +5,11 @@ CREATE TABLE users (
     email TEXT NOT NULL UNIQUE,
     zid TEXT,
     name TEXT NOT NULL,
-    pronouns TEXT NOT NULL,
-    gender TEXT NOT NULL,
+    pronouns TEXT,
+    gender TEXT,
     degree_name TEXT,
     degree_starting_year INTEGER,
-    role user_role NOT NULL,
+    role user_role NOT NULL DEFAULT 'User',
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
