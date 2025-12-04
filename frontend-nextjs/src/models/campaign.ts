d3e5c79ffdfe47c8b1f0dfe02a7bba3722360b1c
ChristianMtnz <christian.mtnz565@gmail.com>
2025-12-05 00:30:20 +1100

    Created campaign webpage utilising CampaignDetails struct

--- frontend-nextjs/src/models/campaign.ts
+++ frontend-nextjs/src/models/campaign.ts
@@ -37,3 +37,8 @@ export async function getCampaignRoles(campaignId: string): Promise<CampaignRole
 export async function getCampaignApplications(campaignId: string): Promise<ApplicationDetails[]> {
     return await apiRequest<ApplicationDetails[]>(`/api/v1/campaign/${campaignId}/applications`);
 }
+
+export async function getCampaignDetails(orgSlug: string, campaignSlug: string): Promise<CampaignDetails> {
+    // return await apiRequest<CampaignDetails>(`/api/v1/campaign/${orgSlug}/${campaignSlug}`);
+    return await apiRequest<CampaignDetails>(`/api/v1/organisation/slug/${orgSlug}/campaign/slug/${campaignSlug}`);
+}
