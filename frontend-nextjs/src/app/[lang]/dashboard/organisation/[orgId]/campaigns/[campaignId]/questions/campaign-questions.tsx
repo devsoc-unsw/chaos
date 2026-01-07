5e6b8b9817a21796275c89e650104eae5e51f291
Kavika <kbpalletenne@gmail.com>
2026-01-07 23:15:44 +1100

    add new questions

--- frontend-nextjs/src/app/[lang]/dashboard/organisation/[orgId]/campaigns/[campaignId]/questions/campaign-questions.tsx
+++ frontend-nextjs/src/app/[lang]/dashboard/organisation/[orgId]/campaigns/[campaignId]/questions/campaign-questions.tsx
@@ -3,10 +3,10 @@
 import { dateToString } from "@/lib/utils";
 import { getCampaign, getCampaignRoles, RoleDetails } from "@/models/campaign";
 import { useQuery } from "@tanstack/react-query";
-import { ArrowLeft, GripVertical, X } from "lucide-react";
+import { ArrowLeft, GripVertical, Plus, X } from "lucide-react";
 import Link from "next/link";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { getAllCommonQuestions, getAllRoleQuestions, MultiOptionQuestionOption, Question, updateQuestion } from "@/models/question";
+import { createQuestion, getAllCommonQuestions, getAllRoleQuestions, MultiOptionQuestionOption, Question, QuestionType, updateQuestion } from "@/models/question";
 import { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -14,6 +14,17 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@
 import { DragDropContext, Draggable, Droppable, DropResult } from '@hello-pangea/dnd';
 import { snowflakeGenerator } from "@/lib";
 import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
 
 export default function CampaignQuestions({ campaignId, orgId, dict }: { campaignId: string, orgId: string, dict: any }) {
     const { data: campaign } = useQuery({
@@ -46,9 +57,12 @@ export default function CampaignQuestions({ campaignId, orgId, dict }: { campaig
     const [allRoleQuestions, setAllRoleQuestions] = useState<{ role: RoleDetails, questions: Question[] }[]>(rolesAndQuestions ?? []);
 
     const [changedQuestions, setChangedQuestions] = useState<Question[]>([]);
+    const [newQuestions, setNewQuestions] = useState<Question[]>([]);
 
     const handleQuestionUpdate = (question: Question) => {
-        if (!changedQuestions.some((q) => q.id === question.id)) {
+        if (newQuestions.some((q) => q.id === question.id)) {
+            setNewQuestions(newQuestions.map((q) => q.id === question.id ? question : q));
+        } else if (!changedQuestions.some((q) => q.id === question.id)) {
             setChangedQuestions([...changedQuestions, question]);
         } else {
             setChangedQuestions(changedQuestions.map((q) => q.id === question.id ? question : q));
@@ -62,10 +76,44 @@ export default function CampaignQuestions({ campaignId, orgId, dict }: { campaig
     }
 
     const saveQuestions = async () => {
-        setChangedQuestions([]);
         await Promise.all(changedQuestions.map(async (question) => {
             await updateQuestion(campaignId, question.id, question);
         }))
+        await Promise.all(newQuestions.map(async (question) => {
+            await createQuestion(campaignId, question);
+        }))
+
+        setChangedQuestions([]);
+        setNewQuestions([]);
+    }
+
+    const addNewQuestion = (type: QuestionType, roleId: string) => {
+        const common = roleId === "common";
+        
+        let newQuestion: Question = { id: snowflakeGenerator.generate().toString(), title: "", description: "", roles: [roleId], created_at: new Date().toISOString(), updated_at: new Date().toISOString(), question_type: type, data: { options: [] }, common, required: false };
+        if (type === 'ShortAnswer') {
+            delete (newQuestion as any).data;
+        }
+        
+        if (common) {
+            newQuestion.roles = [];
+            setAllCommonQuestions([...allCommonQuestions, newQuestion]);
+        } else {
+            setAllRoleQuestions(allRoleQuestions.map(({ role, questions }) => {
+                if (role.id === roleId) {
+                    return { role, questions: [...questions, newQuestion] };
+                }
+    
+                return { role, questions };
+            }));
+        }
+
+        setNewQuestions([...newQuestions, newQuestion]);
+    }
+
+    const addExistingQuestion = (questionId: string, roleId: string) => {
+        // Common questions cannot be shared with roles
+        if (roleId === "common") { return; }
     }
 
     return (
@@ -91,18 +139,75 @@ export default function CampaignQuestions({ campaignId, orgId, dict }: { campaig
                                 <TabsTrigger key={role.id} value={role.id}>{role.name}</TabsTrigger>
                             ))}
                         </TabsList>
-                        <Button disabled={changedQuestions.length === 0} onClick={saveQuestions}>Save</Button>
+                        <Button disabled={changedQuestions.length === 0 && newQuestions.length === 0} onClick={saveQuestions}>Save</Button>
                     </div>
                     <TabsContent value="common">
                         <QuestionEditor campaignId={campaignId} questions={allCommonQuestions} handleQuestionUpdate={handleQuestionUpdate} dict={dict} />
+                        <NewQuestionButton onAddNew={(type) => addNewQuestion(type, "common")} onAddExisting={(questionId) => {}} disableExisting={true} dict={dict} />
                     </TabsContent>
                     {allRoleQuestions?.map(({ role, questions }) => (
                         <TabsContent key={role.id} value={role.id}>
                             <QuestionEditor campaignId={campaignId} possibleRole={role} questions={questions} handleQuestionUpdate={handleQuestionUpdate} dict={dict} />
+                            <NewQuestionButton onAddNew={(type) => addNewQuestion(type, role.id)} onAddExisting={(questionId) => addExistingQuestion(questionId, role.id)} dict={dict} />
                         </TabsContent>
                     ))}
                 </Tabs>
             </div>
+        </div>
+    );
+}
+
+function NewQuestionButton({ onAddNew, onAddExisting, disableExisting = false, dict }: { onAddNew: (type: QuestionType) => void, onAddExisting: (questionId: string) => void, disableExisting?: boolean, dict: any }) {
+    return (
+        <div className="flex justify-center mt-4">
+            <Dialog>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost"><Plus className="w-4 h-4" /></Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuItem onClick={() => onAddNew("ShortAnswer")}>{dict.common.question_types.short_answer}</DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => onAddNew("MultiChoice")}>{dict.common.question_types.multi_choice}</DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => onAddNew("MultiSelect")}>{dict.common.question_types.multi_select}</DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => onAddNew("DropDown")}>{dict.common.question_types.dropdown}</DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => onAddNew("Ranking")}>{dict.common.question_types.ranking}</DropdownMenuItem>
+                        {!disableExisting && (
+                            <>
+                                <DropdownMenuSeparator />
+                                <DialogTrigger>
+                                    <DropdownMenuItem onClick={() => onAddExisting("1")}>{dict.common.question_types.existing_question}</DropdownMenuItem>
+                                </DialogTrigger>
+                            </>
+                        )}
+                    </DropdownMenuContent>
+                </DropdownMenu>
+
+                <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                        <DialogTitle>Edit profile</DialogTitle>
+                        <DialogDescription>
+                            Make changes to your profile here. Click save when you&apos;re
+                            done.
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div className="grid gap-4">
+                        <div className="grid gap-3">
+                            <Label htmlFor="name-1">Name</Label>
+                            <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
+                        </div>
+                        <div className="grid gap-3">
+                            <Label htmlFor="username-1">Username</Label>
+                            <Input id="username-1" name="username" defaultValue="@peduarte" />
+                        </div>
+                    </div>
+                    <DialogFooter>
+                        <DialogClose asChild>
+                            <Button variant="outline">Cancel</Button>
+                        </DialogClose>
+                        <Button type="submit">Save changes</Button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
         </div>
     );
 }
@@ -160,11 +265,22 @@ function MultiOptionQuestionCard({ question, handleQuestionUpdate, dict }: { que
         handleQuestionUpdate({ ...question!, data: { options: newItems } });
     }
 
+
+    const updateTitle = (title: string) => {
+        setTitle(title);
+        handleQuestionUpdate({ ...question!, title: title });
+    }
+
+    const updateQuestionType = (questionType: string) => {
+        setQuestionType(questionType);
+        handleQuestionUpdate({ ...question!, question_type: questionType as QuestionType });
+    }
+
     return (
         <div className="flex flex-col p-2 border rounded-md gap-2 w">
             <div className="flex items-center justify-between gap-2">
-                <Input className="max-w-[500px]" value={title} onChange={(e) => setTitle(e.target.value)} />
-                <Select value={questionType} onValueChange={(value) => setQuestionType(value)}>
+                <Input className="max-w-[500px]" value={title} onChange={(e) => updateTitle(e.target.value)} />
+                <Select value={questionType} onValueChange={(value) => updateQuestionType(value)}>
                     <SelectTrigger>
                         <SelectValue placeholder={dict.common.question_type} />
                     </SelectTrigger>
@@ -249,9 +365,14 @@ function OptionDecorator({ questionType, index }: { questionType: string, index:
 function ShortAnswerQuestionCard({ question, handleQuestionUpdate, dict }: { question?: Question, handleQuestionUpdate: (question: Question) => void, dict: any }) {
     const [title, setTitle] = useState(question?.title ?? "");
 
+    const updateTitle = (title: string) => {
+        setTitle(title);
+        handleQuestionUpdate({ ...question!, title: title });
+    }
+
     return (
         <div className="flex flex-col justify-between p-2 border rounded-md gap-2 min-h-[120px]">
-            <Input className="max-w-[500px]" value={title} onChange={(e) => setTitle(e.target.value)} />
+            <Input className="max-w-[500px]" value={title} onChange={(e) => updateTitle(e.target.value)} />
             <div className="flex flex-col gap-1 p-2">
                 <div className="border-b-2 border-dotted border-gray-500 max-w-[300px]">
                     <p className="text-sm text-gray-500">{dict.dashboard.campaigns.questions.answer_text}</p>
