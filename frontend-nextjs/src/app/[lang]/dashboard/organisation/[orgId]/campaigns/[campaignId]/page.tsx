e85cc156146cebeeea6641ca7afd038653229554
Jared Lucas Amistad Schulz <gorillacyborgz@gmail.com>
2026-01-08 23:28:41 +1100

    fix: cleanup file

--- frontend-nextjs/src/app/[lang]/dashboard/organisation/[orgId]/campaigns/[campaignId]/page.tsx
+++ frontend-nextjs/src/app/[lang]/dashboard/organisation/[orgId]/campaigns/[campaignId]/page.tsx
@@ -27,20 +27,9 @@ import html from 'remark-html';
 
     const campaign = await getCampaign(campaignId);
 
-<<<<<<< ours
-    const processedDescription = await remark()
-        .use(html)
-        .process(campaign.description ?? "");
-    const descriptionHtml = processedDescription.toString();
-    
-    return (
-      <HydrationBoundary state={dehydrate(queryClient)}>
-          <CampaignDetails campaignId={campaignId} descriptionHtml={descriptionHtml} orgId={campaign.organisation_id} dict={dict} />
-=======
     return (
       <HydrationBoundary state={dehydrate(queryClient)}>
           <CampaignDetails campaignId={campaignId} orgId={campaign.organisation_id} dict={dict} />
->>>>>>> theirs
       </HydrationBoundary>
     );
   }
