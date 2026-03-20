74981620412c7270866c7c506accb676297bf65c
Jared Lucas Amistad Schulz <gorillacyborgz@gmail.com>
2026-03-20 22:02:43 +1100

    feat: comments backend

---
+++ backend/migrations/20260320095117_comments.sql
@@ -0,0 +1,18 @@
+-- Add migration script here
+CREATE TABLE comments (
+    id BIGINT PRIMARY KEY,
+    body TEXT NOT NULL,
+    author_id BIGINT NOT NULL,
+    application_id BIGINT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT FK_comments_author
+        FOREIGN KEY (author_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT FK_comments_application
+        FOREIGN KEY (application_id)
+        REFERENCES applications(id)
+        ON DELETE CASCADE
+);
