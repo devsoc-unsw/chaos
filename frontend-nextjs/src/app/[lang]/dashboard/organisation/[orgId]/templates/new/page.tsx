87c80a427295bb61393b0c9eb11bee163a3036c5
Giselle Angela Hanjaya Putri <z5491414@ad.unsw.edu.au>
2026-03-29 12:32:42 +1100

    changed language mode from Django to TypeScript JSX

--- frontend-nextjs/src/app/[lang]/dashboard/organisation/[orgId]/templates/new/page.tsx
+++ frontend-nextjs/src/app/[lang]/dashboard/organisation/[orgId]/templates/new/page.tsx
@@ -1,17 +1,23 @@
-import { getDictionary } from "@/app/[lang]/dictionaries"
-import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query"
+import { getDictionary } from "@/app/[lang]/dictionaries";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 import TemplateNewForm from "./new-template";
 
-
-export default async function TemplateNewPage({ params }: { params: Promise<{ orgId: string, lang: string }> }) {
+export default async function TemplateNewPage({
+  params,
+}: {
+  params: Promise<{ orgId: string; lang: string }>;
+}) {
   const { orgId, lang } = await params;
   const dict = await getDictionary(lang);
   const queryClient = new QueryClient();
-
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
       <TemplateNewForm orgId={orgId} dict={dict} />
     </HydrationBoundary>
-  )
+  );
 }
--- frontend-nextjs/src/app/[lang]/dashboard/organisation/[orgId]/templates/templates.tsx
+++ frontend-nextjs/src/app/[lang]/dashboard/organisation/[orgId]/templates/templates.tsx
@@ -1,13 +1,27 @@
 "use client";
 
-import type { EmailTemplate } from "@/models/email"
-import { useState } from "react"
-import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
-import { Copy, Eye, Pencil, Plus, Trash } from "lucide-react"
-import { deleteEmailTemplate, duplicateEmailTemplate, getOrganisationEmailTemplates, templateVariables } from "@/models/email"
+import type { EmailTemplate } from "@/models/email";
+import { useState } from "react";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import { Copy, Eye, Pencil, Plus, Trash } from "lucide-react";
+import {
+  deleteEmailTemplate,
+  duplicateEmailTemplate,
+  getOrganisationEmailTemplates,
+  templateVariables,
+} from "@/models/email";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { Button } from "@/components/ui/button";
-import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import {
   AlertDialog,
   AlertDialogCancel,
@@ -17,89 +31,118 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
   AlertDialogTrigger,
-} from "@/components/ui/alert-dialog"
+} from "@/components/ui/alert-dialog";
 import {
   Dialog,
   DialogContent,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
+} from "@/components/ui/dialog";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
-export default function EmailTemplates({ orgId, dict }: { orgId: string, dict: any }) {
+export default function EmailTemplates({
+  orgId,
+  dict,
+}: {
+  orgId: string;
+  dict: any;
+}) {
   const router = useRouter();
-  
+
   let { data: templates } = useQuery({
     queryKey: [`${orgId}-email-templates`],
     queryFn: () => getOrganisationEmailTemplates(orgId),
-  })
+  });
 
-  if (!templates) { templates = []; }
-
+  if (!templates) {
+    templates = [];
+  }
 
   return (
     <div className="flex flex-col gap-2">
       <h1 className="text-2xl font-bold">{dict.dashboard.email_templates}</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2">
         {templates.map((template) => {
-          return <EmailCard key={template.id} template={template} dict={dict} />
+          return (
+            <EmailCard key={template.id} template={template} dict={dict} />
+          );
         })}
-        <Card className="flex items-center justify-center gap-1 text-gray-500 hover:text-gray-700 hover:bg-gray-100 cursor-pointer" onClick={() => router.push(`/dashboard/organisation/${orgId}/templates/new`)}>
+        <Card
+          className="flex items-center justify-center gap-1 text-gray-500 hover:text-gray-700 hover:bg-gray-100 cursor-pointer"
+          onClick={() =>
+            router.push(`/dashboard/organisation/${orgId}/templates/new`)
+          }
+        >
           <Plus className="w-10 h-10" />
           <p className="text-xl font-semibold">{dict.dashboard.actions.new}</p>
         </Card>
       </div>
-
     </div>
-  )
+  );
 }
 
-function EmailCard({ template, dict }: { template: EmailTemplate, dict: any }) {
+function EmailCard({ template, dict }: { template: EmailTemplate; dict: any }) {
   const queryClient = useQueryClient();
 
-  const [loadingDuplicate, setLoadingDuplicate] = useState(false)
-  const [loadingDelete, setLoadingDelete] = useState(false)
+  const [loadingDuplicate, setLoadingDuplicate] = useState(false);
+  const [loadingDelete, setLoadingDelete] = useState(false);
 
   const handleDuplicateTemplate = async (templateId: string) => {
     setLoadingDuplicate(true);
     await duplicateEmailTemplate(templateId);
-    await queryClient.invalidateQueries({ queryKey: [`${template.organisation_id}-email-templates`] });
+    await queryClient.invalidateQueries({
+      queryKey: [`${template.organisation_id}-email-templates`],
+    });
     setLoadingDuplicate(false);
-  }
+  };
 
   const handleDeleteTemplate = async (templateId: string) => {
     setLoadingDelete(true);
     await deleteEmailTemplate(templateId);
-    await queryClient.invalidateQueries({ queryKey: [`${template.organisation_id}-email-templates`] });
+    await queryClient.invalidateQueries({
+      queryKey: [`${template.organisation_id}-email-templates`],
+    });
     setLoadingDelete(false);
-  }
+  };
 
   return (
     <Card>
       <CardHeader>
         <div>
-          <p className="text-sm font-semibold text-gray-700">{dict.dashboard.email.name}:</p>
+          <p className="text-sm font-semibold text-gray-700">
+            {dict.dashboard.email.name}:
+          </p>
           <p className="text-2xl line-clamp-1">{template.name}</p>
         </div>
       </CardHeader>
       <CardContent className="flex flex-col gap-2">
         <div>
-          <p className="text-sm font-semibold text-gray-700">{dict.dashboard.email.subject}:</p>
+          <p className="text-sm font-semibold text-gray-700">
+            {dict.dashboard.email.subject}:
+          </p>
           <p className="line-clamp-2">{template.template_subject}</p>
         </div>
         <div>
-          <p className="text-sm font-semibold text-gray-700">{dict.dashboard.email.body}:</p>
-          <p className="line-clamp-4 whitespace-pre-wrap">{template.template_body}</p>
+          <p className="text-sm font-semibold text-gray-700">
+            {dict.dashboard.email.body}:
+          </p>
+          <p className="line-clamp-4 whitespace-pre-wrap">
+            {template.template_body}
+          </p>
         </div>
       </CardContent>
       <CardFooter className="flex gap-1 justify-end mt-auto">
         <EmailTemplatePreview template={template} dict={dict} />
         <Tooltip>
           <TooltipTrigger asChild>
-            <Link href={`/dashboard/organisation/${template.organisation_id}/templates/${template.id}/edit`}>
-              <Button variant="outline" className="items-center"><Pencil className="w-4 h-4" /></Button>
+            <Link
+              href={`/dashboard/organisation/${template.organisation_id}/templates/${template.id}/edit`}
+            >
+              <Button variant="outline" className="items-center">
+                <Pencil className="w-4 h-4" />
+              </Button>
             </Link>
           </TooltipTrigger>
           <TooltipContent>
@@ -108,7 +151,14 @@ function EmailCard({ template, dict }: { template: EmailTemplate, dict: any }) {
         </Tooltip>
         <Tooltip>
           <TooltipTrigger asChild>
-            <Button variant="outline" className="items-center" onClick={() => handleDuplicateTemplate(template.id)} disabled={loadingDuplicate}><Copy className="w-4 h-4" /></Button>
+            <Button
+              variant="outline"
+              className="items-center"
+              onClick={() => handleDuplicateTemplate(template.id)}
+              disabled={loadingDuplicate}
+            >
+              <Copy className="w-4 h-4" />
+            </Button>
           </TooltipTrigger>
           <TooltipContent>
             <p>{dict.dashboard.actions.duplicate}</p>
@@ -118,7 +168,13 @@ function EmailCard({ template, dict }: { template: EmailTemplate, dict: any }) {
           <Tooltip>
             <TooltipTrigger asChild>
               <AlertDialogTrigger asChild>
-                <Button variant="destructive" className="items-center" disabled={loadingDelete}><Trash className="w-4 h-4" /></Button>
+                <Button
+                  variant="destructive"
+                  className="items-center"
+                  disabled={loadingDelete}
+                >
+                  <Trash className="w-4 h-4" />
+                </Button>
               </AlertDialogTrigger>
             </TooltipTrigger>
             <TooltipContent>
@@ -127,14 +183,24 @@ function EmailCard({ template, dict }: { template: EmailTemplate, dict: any }) {
           </Tooltip>
           <AlertDialogContent>
             <AlertDialogHeader>
-              <AlertDialogTitle>{dict.dashboard.actions.destructive_confirm}</AlertDialogTitle>
+              <AlertDialogTitle>
+                {dict.dashboard.actions.destructive_confirm}
+              </AlertDialogTitle>
               <AlertDialogDescription>
                 {dict.dashboard.email.delete_confirmation}
               </AlertDialogDescription>
             </AlertDialogHeader>
             <AlertDialogFooter>
-              <AlertDialogCancel>{dict.dashboard.actions.cancel}</AlertDialogCancel>
-              <Button variant="destructive" onClick={() => handleDeleteTemplate(template.id)} disabled={loadingDelete}>{dict.dashboard.actions.delete}</Button>
+              <AlertDialogCancel>
+                {dict.dashboard.actions.cancel}
+              </AlertDialogCancel>
+              <Button
+                variant="destructive"
+                onClick={() => handleDeleteTemplate(template.id)}
+                disabled={loadingDelete}
+              >
+                {dict.dashboard.actions.delete}
+              </Button>
             </AlertDialogFooter>
           </AlertDialogContent>
         </AlertDialog>
@@ -143,10 +209,22 @@ function EmailCard({ template, dict }: { template: EmailTemplate, dict: any }) {
   );
 }
 
-function EmailTemplatePreview({ template, dict }: { template: EmailTemplate, dict: any }) {
+function EmailTemplatePreview({
+  template,
+  dict,
+}: {
+  template: EmailTemplate;
+  dict: any;
+}) {
   for (const variable of templateVariables) {
-    template.template_body = template.template_body.replace(variable.key, variable.example);
-    template.template_subject = template.template_subject.replace(variable.key, variable.example);
+    template.template_body = template.template_body.replace(
+      variable.key,
+      variable.example
+    );
+    template.template_subject = template.template_subject.replace(
+      variable.key,
+      variable.example
+    );
   }
 
   return (
@@ -155,7 +233,9 @@ function EmailTemplatePreview({ template, dict }: { template: EmailTemplate, dic
         <Tooltip>
           <TooltipTrigger asChild>
             <DialogTrigger asChild>
-              <Button variant="outline"><Eye className="w-4 h-4" /></Button>
+              <Button variant="outline">
+                <Eye className="w-4 h-4" />
+              </Button>
             </DialogTrigger>
           </TooltipTrigger>
           <TooltipContent>
@@ -168,17 +248,21 @@ function EmailTemplatePreview({ template, dict }: { template: EmailTemplate, dic
           </DialogHeader>
           <div className="grid gap-2">
             <div className="flex flex-col">
-              <p className="text-sm font-semibold text-gray-700">{dict.dashboard.email.subject}:</p>
+              <p className="text-sm font-semibold text-gray-700">
+                {dict.dashboard.email.subject}:
+              </p>
               <p className="whitespace-pre-wrap">{template.template_subject}</p>
             </div>
 
             <div className="flex flex-col">
-              <p className="text-sm font-semibold text-gray-700">{dict.dashboard.email.body}:</p>
+              <p className="text-sm font-semibold text-gray-700">
+                {dict.dashboard.email.body}:
+              </p>
               <p className="whitespace-pre-wrap">{template.template_body}</p>
             </div>
           </div>
         </DialogContent>
       </form>
     </Dialog>
-  )
+  );
 }
