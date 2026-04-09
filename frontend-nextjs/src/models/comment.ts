54ef5bd1e4cc399d0821e4eb02813c3ab0a594ae
MaskdDev <maskddev@gmail.com>
2026-04-09 17:29:22 +1000

    feat: Added frontend API wrappers for comments

---
+++ frontend-nextjs/src/models/comment.ts
@@ -0,0 +1,67 @@
+import {apiRequest} from "@/lib";
+
+export interface CommentDetails {
+  id: string;
+  name: string;
+  body: string;
+  author_id: string;
+  created_at: string;
+}
+
+
+export async function createComment(
+  applicationId: string,
+  body: string,
+): Promise<{ comment_id: string }> {
+  return await apiRequest<{ comment_id: string }>(
+    `/api/v1/application/${applicationId}/comment`,
+    {
+      method: "POST",
+      body: {
+        body
+      }
+    },
+  );
+}
+
+
+export async function editComment(
+  applicationId: string,
+  commentId: string,
+  body: string,
+): Promise<void> {
+  await apiRequest(
+    `/api/v1/application/${applicationId}/comment/${commentId}`,
+    {
+      method: "PUT",
+      body: {
+        body
+      }
+    },
+  );
+}
+
+
+export async function deleteComment(
+  applicationId: string,
+  commentId: string,
+): Promise<void> {
+  await apiRequest(
+    `/api/v1/application/${applicationId}/comment/${commentId}`,
+    {
+      method: "DELETE",
+    },
+  );
+}
+
+
+export async function getCommentsByApplication(
+  applicationId: string,
+): Promise<CommentDetails[]> {
+  return await apiRequest<CommentDetails[]>(
+    `/api/v1/application/${applicationId}/comment/`,
+    {
+      method: "GET",
+    },
+  );
+}
