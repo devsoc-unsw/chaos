1e3a51ea4a8125da66b196fc81e72f8939e09d97
Jared Lucas Amistad Schulz <gorillacyborgz@gmail.com>
2026-05-19 13:23:21 +0800

    style: undo unnecessary changes

--- frontend-nextjs/src/models/answer.ts
+++ frontend-nextjs/src/models/answer.ts
@@ -1,48 +1,31 @@
 import { apiRequest } from "@/lib";
 
 export interface Answer {
-  id: string;
-  question_id: string;
-  answer_type: AnswerType;
-  answer_data: AnswerData;
-  created_at: string;
-  updated_at: string;
+    id: string,
+    question_id: string,
+    answer_type: AnswerType,
+    answer_data: AnswerData,
+    created_at: string,
+    updated_at: string,
 }
 
-export type AnswerType =
-  | "ShortAnswer"
-  | "MultiChoice"
-  | "MultiSelect"
-  | "DropDown"
-  | "Ranking";
+export type AnswerType = "ShortAnswer" | "MultiChoice" | "MultiSelect" | "DropDown" | "Ranking";
 
 export type AnswerData = string | string[];
 
-export async function getAllCommonAnswers(
-  applicationId: string,
-): Promise<Answer[]> {
-  return await apiRequest<Answer[]>(
-    `/api/v1/application/${applicationId}/answers/common`,
-  );
+export async function getAllCommonAnswers(applicationId: string): Promise<Answer[]> {
+    return await apiRequest<Answer[]>(`/api/v1/application/${applicationId}/answers/common`);
 }
 
-export async function getAllRoleAnswers(
-  applicationId: string,
-  roleId: string,
-): Promise<Answer[]> {
-  return await apiRequest<Answer[]>(
-    `/api/v1/application/${applicationId}/role/${roleId}/answers`,
-  );
+export async function getAllRoleAnswers(applicationId: string, roleId: string): Promise<Answer[]> {
+    return await apiRequest<Answer[]>(`/api/v1/application/${applicationId}/role/${roleId}/answers`);
 }
 
-export async function updateApplicationRoles(
-  applicationId: string,
-  roles: any,
-): Promise<void> {
+export async function updateApplicationRoles(applicationId: string, roles: any): Promise<void> {
   return await apiRequest<void>(`/api/v1/application/${applicationId}/roles`, {
     method: "PATCH",
     body: {
-      roles,
+      roles
     },
   });
 }
@@ -54,17 +37,11 @@ export async function updateAnswer(answerId: string, body: any): Promise<void> {
   });
 }
 
-export function createAnswer(
-  applicationId: string,
-  body: any,
-): Promise<{ id: string | number }> {
-  return apiRequest<{ id: string | number }>(
-    `/api/v1/application/${applicationId}/answer`,
-    {
-      method: "POST",
-      body,
-    },
-  );
+export function createAnswer(applicationId: string, body: any) {
+  return apiRequest(`/api/v1/application/${applicationId}/answer`, {
+    method: "POST",
+    body,
+  });
 }
 
 export async function deleteAnswer(answerId: string): Promise<void> {
