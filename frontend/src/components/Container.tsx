c4fd72d3d654ae114e6cb1ce6295f87e8e409aa7
Giselle Angela Hanjaya Putri <z5491414@ad.unsw.edu.au>
2026-06-27 14:56:44 +1000

    chore: remove old frontend

--- frontend/src/components/Container.tsx
+++
@@ -1,3 +0,0 @@
-import tw from "twin.macro";
-
-export default tw.div`mx-auto flex max-w-7xl flex-1 flex-col p-4`;
--- frontend/src/utils/constants.ts
+++
@@ -1,2 +0,0 @@
-export const SIGNUP_REQUIRED = "SignupRequired";
-export const HTTP_STATUS_OKAY = 200;
