8895ea24e644677ac41d7d3857de9a103697fcb0
Giselle Angela <134270426+michimochi25@users.noreply.github.com>
2026-06-27 15:23:55 +1000

    Nuke old frontend (#799)
    * set max width on the tooltip
    
    * chore: remove old frontend

--- frontend/src/contexts/SetNavbarTitleContext.ts
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-export const SetNavBarTitleContext = createContext((_title: string) => {});
