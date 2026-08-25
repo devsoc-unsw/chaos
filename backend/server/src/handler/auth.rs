a7fbd912f4aeb73287a037988504eef93cb893fc
Kavika <kavika.palletenne@devsoc.app>
2026-08-25 22:29:21 +1000

    create DBTransaction with extractor in auth.rs

--- backend/server/src/handler/auth.rs
+++ backend/server/src/handler/auth.rs
@@ -94,6 +94,7 @@ pub async fn google_auth_init(
 pub async fn google_callback(
     State(mut state): State<AppState>,
     jar: CookieJar,
+    mut transaction: DBTransaction<'_>,
     Query(query): Query<AuthRequest>,
 ) -> Result<impl IntoResponse, ChaosError> {
     let token = state
@@ -110,8 +111,6 @@ pub async fn google_callback(
         .await?;
 
     let profile = profile.json::<GoogleUserProfile>().await?;
-
-    let mut transaction = DBTransaction::new(&state).await?;
 
     let user_id = create_or_get_user_id(
         profile.email.clone(),
