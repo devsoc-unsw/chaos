a4e4398b3ff541e2d9c46e0c2e468978ad4f6a63
Jared Lucas Amistad Schulz <gorillacyborgz@gmail.com>
2026-09-26 03:05:45 +1000

    fix: restore create role gate

--- backend/server/src/models/campaign.rs
+++ backend/server/src/models/campaign.rs
@@ -631,10 +631,11 @@ impl Campaign {
         snowflake_generator: &mut SnowflakeIdGenerator,
     ) -> Result<i64, ChaosError> {
 
-        // let campaign = Self::get(campaign_id, transaction).await?;
-        // if campaign.published {
-        //     return Err(ChaosError::BadRequest);
-        // }
+        let campaign = Self::get(campaign_id, transaction).await?;
+        if campaign.published {
+            return Err(ChaosError::BadRequest);
+        }
+
         Role::create(campaign_id, role_data, transaction, snowflake_generator).await
     }
 }
